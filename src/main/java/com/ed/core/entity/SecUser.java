20d640bfd102b4d50f3b2d6d1a5bbdb35a464963
Ayman Edris <ayman.edris1986@gmail.com>
2023-06-07 10:06:23 +0300

    c

--- src/main/java/com/ed/core/entity/SecUser.java
+++ src/main/java/com/ed/core/entity/SecUser.java
@@ -6,11 +6,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
 @Entity
 @Table(name = "SEC_USERS")
 public class SecUser {
@@ -24,22 +28,81 @@ public class SecUser {
     @Column(name = "USER_NAME", nullable = false, length = 200)
     private String userName;
 
-    @Size(max = 1000)
-    @NotNull
-    @Column(name = "FULL_NAME", nullable = false, length = 1000)
-    private String fullName;
 
     @Size(max = 1000)
     @Column(name = "EMAIL", length = 1000)
     private String email;
 
-    @Size(max = 50)
-    @Column(name = "PHONE1", length = 50)
-    private String phone1;
-
     @Size(max = 1000)
     @NotNull
     @Column(name = "PASSWORD", nullable = false, length = 1000)
     private String password;
+
+    @Size(max = 400)
+    @Column(name = "FULL_NAME_EN", length = 400)
+    private String fullNameEn;
+
+    @Size(max = 400)
+    @Column(name = "FULL_NAME_AR", length = 400)
+    private String fullNameAr;
+
+    @Size(max = 100)
+    @Column(name = "FIRST_NAME_EN", length = 100)
+    private String firstNameEn;
+
+    @Size(max = 100)
+    @Column(name = "FIRST_NAME_AR", length = 100)
+    private String firstNameAr;
+
+    @Size(max = 100)
+    @Column(name = "SECOND_NAME_EN", length = 100)
+    private String secondNameEn;
+
+    @Size(max = 100)
+    @Column(name = "SECOND_NAME_AR", length = 100)
+    private String secondNameAr;
+
+    @Size(max = 100)
+    @Column(name = "THIRD_NAME_EN", length = 100)
+    private String thirdNameEn;
+
+    @Size(max = 100)
+    @Column(name = "THIRD_NAME_AR", length = 100)
+    private String thirdNameAr;
+
+    @Size(max = 100)
+    @Column(name = "FOURTH_NAME_EN", length = 100)
+    private String fourthNameEn;
+
+    @Size(max = 100)
+    @Column(name = "FOURTH_NAME_AR", length = 100)
+    private String fourthNameAr;
+
+    @Size(max = 50)
+    @Column(name = "PHONE", length = 50)
+    private String phone;
+
+    @Size(max = 50)
+    @Column(name = "MOBILE", length = 50)
+    private String mobile;
+
+    @Column(name = "DATE_OF_BIRTH")
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    @Column(name = "CREATED_AT", nullable = false)
+    private LocalDate createdAt;
+
+    @Size(max = 100)
+    @NotNull
+    @Column(name = "CREATED_BY", nullable = false, length = 100)
+    private String createdBy;
+
+    @Column(name = "UPDATED_AT")
+    private LocalDate updatedAt;
+
+    @Size(max = 100)
+    @Column(name = "UPDATED_BY", length = 100)
+    private String updatedBy;
 
 }
--- src/main/java/com/ed/core/exception/ExceptionHandler.java
+++ src/main/java/com/ed/core/exception/ExceptionHandler.java
@@ -14,18 +14,21 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 public class ExceptionHandler {
     @org.springframework.web.bind.annotation.ExceptionHandler({ValidationException.class})
     public ResponseEntity<ApiResponse<ValidationErrorsDTO>> handleOtherExceptions(ValidationException e , HttpServletRequest request) {
+        e.printStackTrace();
         log.info(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(HttpStatus.BAD_REQUEST,ErrorTypes.VALIDATION_ERROR.toString(),e.getValidationErrorsDTO()));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler({ApiError.class})
     public ResponseEntity<ApiResponse<ApiErrorDTO>> handleOtherExceptions(ApiError e , HttpServletRequest request) {
+        e.printStackTrace();
         log.info(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(HttpStatus.BAD_REQUEST,ErrorTypes.API_ERROR.toString(), ApiErrorDTO.builder().errorCode(e.getErrorCode()).errorMessage(e.getErrorMessage()).build()));
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class,RuntimeException.class})
     public ResponseEntity<ApiResponse<String>> handleOtherExceptions(Exception e , HttpServletRequest request) {
+        e.printStackTrace();
         log.info(e.toString());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiResponse<>(HttpStatus.BAD_REQUEST,ErrorTypes.UNEXPECTED_ERROR.toString(), e.getMessage()));
     }
