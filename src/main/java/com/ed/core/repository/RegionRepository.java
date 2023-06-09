aa2a2d85ba89cd86b182c170ab9c2135e38b7f92
Ayman Edris <ayman.edris1986@gmail.com>
2023-06-09 08:29:28 +0300

    c

--- src/main/java/com/ed/core/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ed.core.repository;
-
-import com.ed.core.entity.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
